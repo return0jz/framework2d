5e27c4596a3633ae18f5b40a40356793d4505d10
JasperZhou <jasperjzhou@gmail.com>
2021-07-22 00:31:43 +0800

    added viewport option

--- graphics/Renderer2D.cpp
+++ graphics/Renderer2D.cpp
@@ -225,10 +225,7 @@ void jzj::Renderer2D::clear(float r, float g, float b, float a) {
     glClearColor(r,g,b,a);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 }
-<<<<<<< HEAD
 
 void jzj::Renderer2D::setViewport(int x, int y, int w, int h) {
     glViewport(x, y, w, h);
 }
-=======
->>>>>>> efe143511a1ce5bf1c0e78e2f05e648568e8aef6
